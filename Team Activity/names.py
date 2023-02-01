b8ff10c5ae7893d69cbd0e48bde4cc270fb5898d
sibervi <vinnicius.castro97@gmail.com>
2023-02-01 12:17:58 -0700

    Team Activity

--- Team Activity/names.py
+++ Team Activity/names.py
@@ -10,7 +10,7 @@ def make_full_name(given_name, family_name):
         family_name: a string that contains a person's family name
     Return: a string in the form "family_name; given_name"
     """
-    full_name = f"{family_name};{given_name}"
+    full_name = f"{family_name}; {given_name}"
     return full_name
 
 
@@ -43,7 +43,7 @@ def extract_given_name(full_name):
     Return: a string that contains a person's given name
     """
     # Find the index where "; " appears within the full name string.
-    semicolon_index = full_name.index("/ ")
+    semicolon_index = full_name.index("; ")
 
     # Extract a substring from the full name and return it.
     given_name = full_name[semicolon_index + 2 : ]
--- Team Activity/test_name.py
+++ Team Activity/test_name.py
@@ -6,12 +6,19 @@ from names import make_full_name, \
 import pytest
 
 def test_make_full_name():
-    pass
+    assert make_full_name("Vinnicius", "Castro") == "Castro; Vinnicius"
+    assert make_full_name("Marie", "Toussaint") == "Toussaint; Marie"
+    assert make_full_name("Oliver", "Toussaint") == "Toussaint; Oliver"
 
 def test_extract_family_name():
-    pass
+    assert extract_family_name("Castro; Vinnicius") == "Castro"
+    assert extract_family_name("Toussaint; Marie") == "Toussaint"
+    assert extract_family_name("Toussaint; Oliver") == "Toussaint"
 def test_extract_given_name():
-    pass
+    assert extract_given_name("Castro; Vinnicius") == "Vinnicius"
+    assert extract_given_name("Toussaint; Marie") == "Toussaint"
+    assert extract_given_name("Toussaint; Oliver") == "Toussaint"
+
 
 
 
