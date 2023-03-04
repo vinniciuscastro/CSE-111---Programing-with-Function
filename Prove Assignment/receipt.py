0bb53d7df6b5111a7f7ddae57b6c94a6859109f1
sibervi <vinnicius.castro97@gmail.com>
2023-03-04 16:44:29 -0700

    prove ready

--- Prove Assignment/receipt.py
+++ Prove Assignment/receipt.py
@@ -3,9 +3,33 @@
 import csv
 def main():
     vc_index = 0
-    vc_name_index = 1
+    product_name = 0
+    requested_quantity = 1
+    product_price = 2
     product_dic = read_dictionary("/Users/vinni/OneDrive/Documents/CSE 111 - Programing with Function/Prove Assignment/products.csv",vc_index)
     print(f"All Products\n{product_dic}")
+    print()
+    print("Requested Items")
+
+    with open("/Users/vinni/OneDrive/Documents/CSE 111 - Programing with Function/Prove Assignment/request.csv", "rt") as vc_requests:
+        vc_reader = csv.reader(vc_requests)
+        next(vc_reader)
+
+        for row in vc_reader:
+            vc_product_name = row[product_name]
+            
+            vc_quantity = int(row[requested_quantity])
+            vc_name = product_dic[vc_product_name][1]
+            vc_individual_price = float(product_dic[vc_product_name][product_price])
+            vc_price = vc_quantity * vc_individual_price
+            print(f"{vc_name}: {vc_quantity} @ {vc_individual_price:.2f}")
+
+            # if vc_product_name not in product_dic:
+            #     print("No such product")
+            
+
+
+
 
 def read_dictionary(filename, key_column_index):
         # Create an empty dictionary that will
